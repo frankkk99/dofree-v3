2030c1136df77673669a14dcbfd7ecdd8f475385
Frank chetsarit <am2.production29@gmail.com>
2026-06-25 02:35:56 +0700

    Load home page from Supabase catalog

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { HomeExperienceV3 } from '@/components/home-experience-v3';
-import { getHomePayload } from '@/lib/tmdb';
+import { getCatalogHomePayload } from '@/lib/catalog-home';
 
 export default async function HomePage() {
-  const home = await getHomePayload();
+  const home = await getCatalogHomePayload();
   return <HomeExperienceV3 home={home} />;
 }
