ff50a0a3c3ab0daee72d943aec9c755249ac913c
Frank chetsarit <am2.production29@gmail.com>
2026-06-25 03:03:31 +0700

    Show realtime recently added carousel on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { HomeExperienceV3 } from '@/components/home-experience-v3';
+import { HomeRealtimeWrapper } from '@/components/home-realtime-wrapper';
 import { getCatalogHomePayload } from '@/lib/catalog-home';
 
 export default async function HomePage() {
   const home = await getCatalogHomePayload();
-  return <HomeExperienceV3 home={home} />;
+  return <HomeRealtimeWrapper home={home} />;
 }
