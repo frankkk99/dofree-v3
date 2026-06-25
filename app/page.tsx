3dae455eedfbd49101202e9d6053a41aff6381f1
Frank chetsarit <am2.production29@gmail.com>
2026-06-25 11:56:09 +0700

    Wire favorite bridge on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { FavoriteClickBridge } from '@/components/favorite-click-bridge';
 import { HomeRealtimeWrapper } from '@/components/home-realtime-wrapper';
 import { getCatalogHomePayload } from '@/lib/catalog-home';
 
 export default async function HomePage() {
   const home = await getCatalogHomePayload();
-  return <HomeRealtimeWrapper home={home} />;
+  return (
+    <>
+      <HomeRealtimeWrapper home={home} />
+      <FavoriteClickBridge home={home} />
+    </>
+  );
 }
