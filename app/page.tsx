5b9043ffb4111891bce30c759caba49a8e550d37
Frank chetsarit <am2.production29@gmail.com>
2026-06-26 01:08:44 +0700

    Add homepage structured data

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,37 @@ import { HomeCardLinkBridge } from '@/components/home-card-link-bridge';
 import { HomeRealtimeWrapper } from '@/components/home-realtime-wrapper';
 import { WatchHistoryClickBridge } from '@/components/watch-history-click-bridge';
 import { getCatalogHomePayload } from '@/lib/catalog-home';
+import { absoluteUrl, seoConfig } from '@/lib/seo';
 
-const siteName = 'ดูดีดี';
-const siteUrl = 'https://www.xn--l3caa5kbu.online/';
-const websiteJsonLd = {
-  '@context': 'https://schema.org',
-  '@type': 'WebSite',
-  name: siteName,
-  alternateName: ['ดูดีดีออนไลน์', 'DooDeeDee'],
-  url: siteUrl,
-};
+const homepageJsonLd = [
+  {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: seoConfig.siteName,
+    alternateName: ['ดูดีดีออนไลน์', seoConfig.englishSiteName, seoConfig.legacyName],
+    url: absoluteUrl('/'),
+    inLanguage: seoConfig.language,
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: `${absoluteUrl('/search')}?q={search_term_string}`,
+      'query-input': 'required name=search_term_string',
+    },
+  },
+  {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: seoConfig.siteName,
+    alternateName: [seoConfig.englishSiteName, seoConfig.legacyName],
+    url: absoluteUrl('/'),
+    logo: absoluteUrl('/opengraph-image'),
+  },
+];
 
 export default async function HomePage() {
   const home = await getCatalogHomePayload();
   return (
     <>
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteJsonLd) }} />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(homepageJsonLd) }} />
       <HomeRealtimeWrapper home={home} />
       <HomeCardLinkBridge home={home} />
       <FavoriteClickBridge home={home} />
