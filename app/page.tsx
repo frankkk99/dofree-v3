c6c7a9bdf34677a06ec022e0c0d4283bc1c247b1
Frank chetsarit <am2.production29@gmail.com>
2026-06-25 12:00:39 +0700

    Wire watch history bridge on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { FavoriteClickBridge } from '@/components/favorite-click-bridge';
 import { HomeRealtimeWrapper } from '@/components/home-realtime-wrapper';
+import { WatchHistoryClickBridge } from '@/components/watch-history-click-bridge';
 import { getCatalogHomePayload } from '@/lib/catalog-home';
 
 export default async function HomePage() {
@@ -8,6 +9,7 @@ export default async function HomePage() {
     <>
       <HomeRealtimeWrapper home={home} />
       <FavoriteClickBridge home={home} />
+      <WatchHistoryClickBridge home={home} />
     </>
   );
 }
