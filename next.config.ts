cb51af30fb51c59ef2652788b4e43b6d449aaf52
Frank chetsarit <am2.production29@gmail.com>
2026-06-30 14:53:31 +0700

    Allow remote image domains in Next config

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
