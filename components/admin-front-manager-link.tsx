4e14bc48f4d0bf2d5e7ef298557044652116e794
Frank chetsarit <am2.production29@gmail.com>
2026-06-30 16:07:59 +0700

    Add front manager topbar link component

---
+++ components/admin-front-manager-link.tsx
@@ -0,0 +1,10 @@
+export function AdminFrontManagerLink() {
+  return (
+    <a
+      href="/admin/front-manager"
+      className="shrink-0 rounded-xl bg-white/[0.08] px-3 py-2 text-xs font-black text-white/82 hover:bg-white/[0.14] hover:text-white md:rounded-2xl md:px-4"
+    >
+      จัดการหน้าบ้าน
+    </a>
+  );
+}
